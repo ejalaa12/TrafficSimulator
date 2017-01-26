d69e6b3974cf8db1874bf6313252c24211c87311
El Jawad Alaa <ejalaa12@gmail.com>
2017-01-26 22:03:57 +0100

    Solution for the traffic jam
    Simply replace a traffic light by a stop :(

--- src/configuration/ConfigTest.java
+++ src/configuration/ConfigTest.java
@@ -21,7 +21,7 @@ public class ConfigTest {
     public static void main(String args[]) throws FileNotFoundException {
         NetworkConfiguration netconf = null;
         Yaml yaml = new Yaml();
-        InputStream input = new FileInputStream(new File("./src/configuration/coruscant.yml"));
+        InputStream input = new FileInputStream(new File("./src/configuration/coruscant_bis.yml"));
         netconf = yaml.loadAs(input, NetworkConfiguration.class);
         System.out.println(netconf);
 
@@ -32,6 +32,7 @@ public class ConfigTest {
         */
         Logger.getInstance().setLogLevel(LogLevel.EVENT);
         Logger.getInstance().turnOff();
+        Logger.getInstance().turnCsvOn();
         LocalDateTime startSim = LocalDateTime.of(2000, 1, 1, 0, 0);
         LocalDateTime endSim = LocalDateTime.of(2000, 1, 2, 0, 3);
 
@@ -39,6 +40,7 @@ public class ConfigTest {
         RoadNetwork network = netconf.buildNetworkFromConfig(simEngine);
         network.init();
         simEngine.loop();
+        Logger.getInstance().close();
 
         System.out.println("Total Produced cars");
         for (Zone zone : network.getZones()) {
